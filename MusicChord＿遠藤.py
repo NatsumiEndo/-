cc1038a06211c79c2fe9d52421f64b4d888fd648
NatsumiEndo <natsumitsuki1814@gmail.com>
2025-06-26 14:00:50 +0900

    Update MusicChord＿遠藤.py

--- MusicChord＿遠藤.py
+++ MusicChord＿遠藤.py
@@ -3,9 +3,12 @@ import streamlit as st
 st.title("数値計算アプリ")
 
 # 入力
-a = st.number_input("Aの値", value=0)
-b = st.number_input("Bの値", value=0)
-c = st.number_input("Cの値", value=1)
+f1 = st.number_input("Aの値", value=0)
+f2 = st.number_input("Bの値", value=0)
+f3 = st.number_input("Cの値", value=1)
+v1 = st.number_input("v1の値", value=1)
+v2 = st.number_input("v2の値", value=1)
+v3 = st.number_input("v3の値", value=1)
 
 # 実行ボタン
 if st.button("計算する"):
